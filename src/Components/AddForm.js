77a0aed2fd4ee59ac7efdcd79065311b7824b3f2
peanutyabing <sherry.liuyabing@gmail.com>
2023-03-31 10:54:24 +0800

    Improve AddForm styling

--- src/Components/AddForm.js
+++ src/Components/AddForm.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import axios from "axios";
-import { Form, Row, Col, Button, ButtonGroup } from "react-bootstrap";
+import {
+  Container,
+  Form,
+  Row,
+  Col,
+  Button,
+  ButtonGroup,
+} from "react-bootstrap";
 
 export default function AddForm(props) {
   const blankForm = {
@@ -32,54 +39,60 @@ export default function AddForm(props) {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Row>
-        <Col></Col>
-        <Col>
-          <Form.Control
-            name="first_name"
-            value={row.first_name}
-            onChange={handleChange}
-          />
-        </Col>
-        <Col>
-          <Form.Control
-            name="last_name"
-            value={row.last_name}
-            onChange={handleChange}
-          />
-        </Col>
-        <Col>
-          <Form.Control
-            name="mobile"
-            value={row.mobile}
-            onChange={handleChange}
-          />
-        </Col>
-        <Col>
-          <Form.Select name="gender" value={row.gender} onChange={handleChange}>
-            <option></option>
-            <option value={false}>Female</option>
-            <option value={true}>Male</option>
-          </Form.Select>
-        </Col>
-        <Col>
-          <ButtonGroup>
-            <Button variant="light" size="sm" type="submit">
-              ✔
-            </Button>
-            <Button
-              variant="secondary"
-              size="sm"
-              onClick={() => {
-                props.hideAddForm();
-              }}
+    <Container className="table">
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col></Col>
+          <Col>
+            <Form.Control
+              name="first_name"
+              value={row.first_name}
+              onChange={handleChange}
+            />
+          </Col>
+          <Col>
+            <Form.Control
+              name="last_name"
+              value={row.last_name}
+              onChange={handleChange}
+            />
+          </Col>
+          <Col>
+            <Form.Control
+              name="mobile"
+              value={row.mobile}
+              onChange={handleChange}
+            />
+          </Col>
+          <Col>
+            <Form.Select
+              name="gender"
+              value={row.gender}
+              onChange={handleChange}
             >
-              ✘
-            </Button>
-          </ButtonGroup>
-        </Col>
-      </Row>
-    </Form>
+              <option></option>
+              <option value={false}>Female</option>
+              <option value={true}>Male</option>
+            </Form.Select>
+          </Col>
+          <Col>
+            <ButtonGroup>
+              <Button variant="light" size="sm" type="submit">
+                ✔
+              </Button>
+              <Button
+                variant="secondary"
+                size="sm"
+                onClick={() => {
+                  props.hideAddForm();
+                }}
+              >
+                ✘
+              </Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
